c9e8932e2e82e385e7a6660873ec23d8dc2f2c28
rick <rickhou44@gmai.com>
2020-03-14 19:16:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,189 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- item沒有使用組件
+    <div>
+        <div>
+            <input type="text" v-model="inputStr" class="input-box" v-on:keyup.13="addItem()">
+            <button class="btn create" @click="addItem()">新增</button>
+        </div>
+        <ul>
+            <li>
+                <div class="li-title">事情</div>
+                <div class="li-title">功能</div>
+            </li>
+            <li v-for="item in list" :key="item.id">
+                <div class="things" @click="toggleFinish(item.id)" :class="{isFinished:item.finished}">{{item.thing}}</div>
+                <div class="function-btn">
+                    <button class="btn delete" @click="deleteItem(item.id)">刪除</button>
+                    <button class="btn finish" @click="toggleFinish(item.id)">完成</button>
+                    <button class="btn modify">更改</button>
+                </div>
+            </li>
+        </ul>
+    </div> 
+    -->
+   <div>
+        <div>
+            <input type="text" v-model="inputStr" class="input-box" v-on:keyup.13="addItem()">
+            <button class="btn create" @click="addItem()">新增</button>
+        </div>
+        <TodoList
+        :todo-list="list"
+        :input-str="inputStr"
+        >
+        </TodoList>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+//import TodoItem from './components/TodoItem.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    TodoList,
+    //TodoItem
+  },
+  data () {
+      return{
+          inputStr:"",
+          list:[
+              {id:0, thing:"吃飯", finished:false, isEdit:false},
+              {id:1, thing:"睡覺", finished:false, isEdit:false},
+              {id:2, thing:"打東東", finished:false, isEdit:false}
+          ],
+      }
+  },
+  methods:{
+    addItem(){
+        let ind = 0;
+        this.list.forEach((item) => {
+            if(ind < item.id){
+                ind = item.id;
+            }
+        });
+        this.list.push({id:ind+1, thing:this.inputStr, finished:false, isEdit:false});
+        this.inputStr = "";
+    }
   }
+  /*
+  methods:{
+      toggleFinish(itemId){
+          this.list.forEach((item) => {
+              if(item.id == itemId){
+                  item.finished = !item.finished;
+              }
+          });
+      },
+      deleteItem(itemId){
+          let newList = this.list;
+          this.list.forEach((item,index) => {
+              if(item.id == itemId){
+                  let id = index;
+                  newList.splice(id, 1);
+              }
+          });
+          this.list = newList;
+      },
+      addItem(){
+          let ind = 0;
+          this.list.forEach((item) => {
+              if(ind < item.id){
+                  ind = item.id;
+              }
+          });
+          this.list.push({id:ind+1, thing:this.inputStr, finished:false});
+          this.inputStr = "";
+      }
+  }
+  */
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: '微軟正黑體', Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.input-box{
+    height:25px;
+    margin:0 10px;
+    font-size: 20px;
+    border-radius: 5px;
+    }
+    .btn{
+        width:100px;
+        height:30px;
+        border-radius:5px;
+        color: aliceblue;
+        font-size:18px;
+        cursor: pointer;
+        font-weight: bolder;
+        font-family: '微軟正黑體', Courier, monospace;
+    }
+    .btn:hover{
+        background-color: aliceblue;
+    }
+    .create{
+        border:3px solid #FFC107;
+        background-color: #FFC107;
+    }
+    .create:hover{
+        color:#FFC107;
+    }
+    ul{ 
+        width:960px;
+        margin:20px auto;
+        font-size: 20px;
+        font-family: '微軟正黑體', Courier, monospace
+    }
+    li{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        padding:15px;
+    }
+    .li-title{
+        font-size: 30px;
+        border-bottom:5px solid #35495E;
+    }
+
+    .function-btn{
+        display: flex;
+        flex-direction: row;
+        justify-content:space-around;
+    }
+    
+    .delete{
+        background-color: #DC3545;
+        border:3px solid #DC3545;
+    }
+    .delete:hover{
+        color:#DC3545;
+    }
+    .finish{
+        background-color: #007BFF;
+        border:3px solid #007BFF;
+    }
+    .finish:hover{
+        color:#007BFF;
+    }
+    .modify{
+        background-color: #17A2B8;
+        border:3px solid #17A2B8;
+    }
+    .modify:hover{
+        color:#17A2B8;
+    }
+    .isFinished{
+        text-decoration:line-through;
+        color:gray;
+    }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,90 @@
+<template>
+    <ul>
+        {{inputStr}}
+        <li>
+            <div class="li-title">事情</div>
+            <div class="li-title">功能</div>
+        </li>
+        <li v-for="item in todoList" :key="item.id">
+            <div v-show="item.isEdit == false">
+                <div class="things" @click="toggleFinish(item.id)" :class="{isFinished:item.finished}">{{item.thing}}</div>
+            </div>
+            
+            <div v-show="item.isEdit == true">
+                <input type="text" v-model="modifyText" class="input-box" 
+                v-on:keyup.13="modifyItem(item.id)">
+            </div>
+
+            <div class="function-btn">
+                <button class="btn delete" @click="deleteItem(item.id)">刪除</button>
+                <button class="btn finish" @click="toggleFinish(item.id)">完成</button>
+                <button class="btn modify" @click="modifyToInput(item.id)">更改</button>
+            </div>
+        </li>
+    </ul>
+</template>
+<script>
+
+export default {
+    name:'TodoList',
+    props:['todoList','inputStr'],
+    data () {
+        return{
+            // 表示有無更改之狀態，
+            // ->若有，按下更改就是更改該項目
+            // ->若無，按下更改就是呼叫更改框出來
+            haveAnyModify:false,
+            modifyText:"",
+        }
+    },
+    methods:{
+      toggleFinish(itemId){
+          this.todoList.forEach((item) => {
+              if(item.id == itemId){
+                  item.finished = !item.finished;
+              }
+          });
+      },
+      deleteItem(itemId){
+          let newList = this.todoList;
+          this.todoList.forEach((item,index) => {
+              if(item.id == itemId){
+                  let id = index;
+                  newList.splice(id, 1);
+              }
+          });
+          this.todoList = newList;
+      },
+      modifyToInput(itemId){
+        if(this.haveAnyModify == false){
+            this.todoList.forEach((item) => {
+            if(item.id == itemId){
+                item.isEdit = !item.isEdit;
+                this.haveAnyModify = !this.haveAnyModify;
+                if(item.isEdit == true){
+                    this.modifyText = item.thing;
+                }
+            }
+        });
+        }else{
+            this.modifyItem(itemId);
+        }
+          
+      },
+      modifyItem(itemId){
+        this.todoList.forEach((item) => {
+              if(item.id == itemId){
+                  item.thing = this.modifyText;
+                  this.modifyText = '';
+                  item.isEdit = !item.isEdit;
+                  this.haveAnyModify = !this.haveAnyModify;
+              }
+          });
+      }
+  }
+}
+</script>
+
+<style>
+    
+</style>
